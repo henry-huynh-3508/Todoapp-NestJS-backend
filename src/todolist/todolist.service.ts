11bb5a15704e1a26e16dbb061cfdb69204f2298d
Henry <huyearly@gmail.com>
2020-06-02 15:16:48 -0600

    Add todolist service

---
+++ src/todolist/todolist.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Todolist } from './todolist.model';
+@Injectable()
+export class TodoListService {
+  todolists: Todolist[] = [];
+
+  addTodoList(tittle: string, desc: string, items: any[]): any {
+    const newTodoList = new Todolist(
+      new Date().toISOString(),
+      tittle,
+      desc,
+      items,
+    );
+    this.todolists.push(newTodoList);
+  }
+}
